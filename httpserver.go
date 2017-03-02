ebfd836143f6d208c03847efefbea046334ae86d
Adel Mirzazhanov <adel.mirzazhanov@gmail.com>
2017-03-02 15:02:24 +0600

    http template added

--- httpserver.go
+++ httpserver.go
@@ -6,21 +6,32 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
+
+// Page structure simple
+type Page struct {
+	Title string
+	Body  []byte
+}
 
 // HandlerImages - handler for /images/ url
 func HandlerImages(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Requested Image Is %s!", r.URL.Path[1:])
 }
 
-// HandlerRSS - handler for /rss/ url
-func HandlerRSS(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Requested RSS is %s!", r.URL.Path[1:])
+// HandlerHTML - handler for /rss/ url
+func HandlerHTML(w http.ResponseWriter, r *http.Request) {
+	pTmp := &Page{Title: "TestPage", Body: []byte("This is a sample simple Page.")}
+	t, _ := template.ParseFiles("template.html")
+	t.Execute(w, pTmp)
+	fmt.Println("Connection-------->")
+	fmt.Println("Connection--->")
 }
 
 func main() {
 	http.HandleFunc("/images/", HandlerImages)
-	http.HandleFunc("/rss/", HandlerRSS)
+	http.HandleFunc("/html/", HandlerHTML)
 	http.ListenAndServe(":8080", nil)
 }
