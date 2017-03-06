c810819f79554fb4363a58dcb348ce11b6d03410
Adel Mirzazhanov <adel.mirzazhanov@gmail.com>
2017-03-06 16:02:07 +0600

    universal logging for http request

--- httpserver.go
+++ httpserver.go
@@ -17,6 +17,14 @@ type Page struct {
 	Body  []byte
 }
 
+// Log function
+func Log(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s\t%s\t%s\t%s\tX-FORWARDED-FOR: %s", r.RemoteAddr, r.Method, r.Proto, r.URL.Path[1:], r.Header.Get("X-FORWARDED-FOR"))
+		handler.ServeHTTP(w, r)
+	})
+}
+
 // HandlerImages - handler for /images/ url
 func HandlerImages(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Requested Image Is %s!", r.URL.Path[1:])
@@ -27,11 +35,10 @@ func HandlerHTML(w http.ResponseWriter, r *http.Request) {
 	pTmp := &Page{Title: "TestPage", Body: []byte("This is a sample simple Page.")}
 	t, _ := template.ParseFiles("template.html")
 	t.Execute(w, pTmp)
-	log.Printf("%s", r.URL.Path[1:])
 }
 
 func main() {
 	http.HandleFunc("/images/", HandlerImages)
 	http.HandleFunc("/html/", HandlerHTML)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", Log(http.DefaultServeMux)))
 }
