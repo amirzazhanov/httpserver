127a740c432ba2439555815bc246b092b7d2e782
Adel Mirzazhanov <adel.mirzazhanov@gmail.com>
2017-03-10 12:46:46 +0600

    server code is splitted in several modules

--- httpserver.go
+++ httpserver.go
@@ -1,12 +1,11 @@
+// Package httpserver to test server implementation
+package main
+
 // https://golang.org/doc/articles/wiki/
 // http://marcio.io/2015/07/handling-1-million-requests-per-minute-with-golang/
 // https://thenewstack.io/make-a-restful-json-api-go/
-//
-package main
 
 import (
-	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 )
@@ -23,18 +22,6 @@ func Log(handler http.Handler) http.Handler {
 		log.Printf("%s\t%s\t%s\t%s\tX-FORWARDED-FOR: %s", r.RemoteAddr, r.Method, r.Proto, r.URL.Path[1:], r.Header.Get("X-FORWARDED-FOR"))
 		handler.ServeHTTP(w, r)
 	})
-}
-
-// HandlerImages - handler for /images/ url
-func HandlerImages(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Requested Image Is %s!", r.URL.Path[1:])
-}
-
-// HandlerHTML - handler for /rss/ url
-func HandlerHTML(w http.ResponseWriter, r *http.Request) {
-	pTmp := &Page{Title: "TestPage", Body: []byte("This is a sample simple Page.")}
-	t, _ := template.ParseFiles("template.html")
-	t.Execute(w, pTmp)
 }
 
 func main() {
